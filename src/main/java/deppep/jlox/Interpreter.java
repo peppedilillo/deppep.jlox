16468af10a5436c3307211a6e71f49fe9ecb1a40
peppedilillo <peppedilillo@gmail.com>
2025-03-03 23:09:22 +0100

    chp7: basic interpreter + extra interpreter class for ternary expressions

---
+++ src/main/java/deppep/jlox/Interpreter.java
@@ -0,0 +1,128 @@
+package deppep.jlox;
+
+
+// Lox objects are stored in Java's objects. Hence the interpreter return these.
+public class Interpreter implements Expr.Visitor<Object> {
+	
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+	@Override
+	public Object visitGroupingExpr(Expr.Grouping expr) {
+		return evaluate(expr.expression);
+	}
+
+	// challenge 6.2: extends
+	@Override
+	public Object visitTernaryExpr(Expr.Ternary expr) {
+		Object left = evaluate(expr.left);
+		if (isTruthy(left)) {
+			Object middle = evaluate(expr.middle);
+			return middle;
+		}
+		Object right = evaluate(expr.right);
+		return right;
+	}
+
+	@Override
+	public Object visitBinaryExpr(Expr.Binary expr) {
+		// note that we are evaluating left to right.
+		// this means that if `left` has a side effect on `right`
+		// that will take place! we will also check the type after having
+		// evaluated both. this is not trivial, we could have evaluated
+		// the first, checked it, then do the same with the second.
+		Object left = evaluate(expr.left);
+		Object right = evaluate(expr.right);
+
+		switch (expr.operator.type) {
+		case TokenType.GREATER:
+			checkNumberOperands(expr.operator, left, right);
+			return (double)left > (double)right;
+		case TokenType.GREATER_EQUAL:
+			checkNumberOperands(expr.operator, left, right);
+			return (double)left >= (double)right;
+		case TokenType.LESS:
+			checkNumberOperands(expr.operator, left, right);
+			return (double)left < (double)right;
+		case TokenType.LESS_EQUAL:
+			checkNumberOperands(expr.operator, left, right);
+			return (double)left <= (double)right;
+		case TokenType.MINUS:
+			checkNumberOperands(expr.operator, left, right);
+			return (double)left - (double)right;
+		case TokenType.PLUS:
+			// in Lox the `+` operator is overloaded to support
+			// both addition and string concatenation.
+			if (left instanceof Double && right instanceof Double) {
+				return (double)left + (double)right;
+			}
+
+			if (left instanceof String && right instanceof String) {
+				return (String)left + (String)right;
+			}
+
+			throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+		case TokenType.SLASH:
+			checkNumberOperands(expr.operator, left, right);
+			return (double)left / (double)right;
+		case TokenType.STAR:
+			checkNumberOperands(expr.operator, left, right);
+			return (double)left * (double)right;
+		case TokenType.BANG_EQUAL:
+			return !isEqual(left, right);
+		case TokenType.EQUAL_EQUAL:
+			return isEqual(left, right);
+		}
+
+		// unreachable
+		return null;
+	}
+
+	@Override
+	public Object visitUnaryExpr(Expr.Unary expr) {
+		Object right = evaluate(expr.right);
+
+		switch (expr.operator.type) {
+		case TokenType.BANG:
+			return !isTruthy(right);
+		case TokenType.MINUS:
+			checkNumberOperand(expr.operator, right);
+			return -(double)right;
+		}
+
+		// unreachable
+		return null;
+	}
+
+	private void checkNumberOperand(Token operator, Object operand) {
+		if (operand instanceof Double) return;
+		throw new RuntimeError(operator, "Operand must be a number");
+	}
+
+	private void checkNumberOperands(Token operator, Object left, Object right) {
+		if (left instanceof Double && right instanceof Double) return;
+		throw new RuntimeError(operator, "Operands must be numbers.");
+	}
+	
+	// here we decide what truth value is given to a certain object
+	// type. Lox follows ruby in this: null and false are "falsey", they
+	// evaluate to false. all the other are "truthy", they evaluate to true.
+	private boolean isTruthy(Object object) {
+		if (object == null) return false;
+		if (object instanceof Boolean) return (boolean)object;
+		return true;
+	}
+
+	private boolean isEqual(Object a, Object b) {
+		if (a == null && b == null) return true;
+		if (a == null) return false;
+
+		return a.equals(b);
+	}	
+
+	private Object evaluate(Expr expr){
+		return expr.accept(this);
+	}
+}
