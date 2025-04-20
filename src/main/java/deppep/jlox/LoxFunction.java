9e67a0df89d856d14699834e70a630f8be2bab76
peppedilillo <peppedilillo@gmail.com>
2025-04-20 19:40:34 +0200

    chapter 10: closures

--- src/main/java/deppep/jlox/LoxFunction.java
+++ src/main/java/deppep/jlox/LoxFunction.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
-    LoxFunction(Stmt.Function declaration) {
+    private final Environment closure;
+
+    LoxFunction(Stmt.Function declaration, Environment closure) {
+        this.closure = closure;
         this.declaration = declaration;
     }
 
@@ -16,7 +19,7 @@ class LoxFunction implements LoxCallable {
         // this enables recursion, which otherwise would not be possible.
         // functions also 'encapsulate' its parameters meaning that these are not visible
         // anywhere else in the code.
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         // we walk the declaration, binding arguments (values) to parameters (lexeme, symbols)
         for (int i=0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme, arguments.get(i));
